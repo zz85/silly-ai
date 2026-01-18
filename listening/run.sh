52ca3ed9eaef02c0f133df48caf41f2ae6924264
Joshua Koo <zz85nus@gmail.com>
2026-01-17 23:14:19 -0800

    Remove unused code for testing audio in

--- listening/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Wrapper to set Swift library path for screencapturekit
-export DYLD_LIBRARY_PATH="/Library/Developer/CommandLineTools/usr/lib/swift-5.5/macosx:$DYLD_LIBRARY_PATH"
-exec "$(dirname "$0")/target/debug/listening" "$@" 2>&1 | grep -v "SwiftNativeNSObject"
